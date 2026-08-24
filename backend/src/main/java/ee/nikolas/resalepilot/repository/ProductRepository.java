423fd5478ab0429dd92ac7c83b343c4c8898d784
Nikolas Arro <arronikolas@gmail.com>
2026-08-24 20:26:48 +0300

    Add Product repository

---
+++ backend/src/main/java/ee/nikolas/resalepilot/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package ee.nikolas.resalepilot.repository;
+
+import ee.nikolas.resalepilot.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Optional<Product> findBySku(String sku);
+
+    boolean existsBySku(String sku);
+}
